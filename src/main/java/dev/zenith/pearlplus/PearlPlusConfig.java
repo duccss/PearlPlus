bf9d7a7f6f67981a7b7592fa13d1dcb217ffd910
duccss <123850042+duccss@users.noreply.github.com>
2025-08-03 04:39:46 +0000

    Create PearlPlusConfig.java

---
+++ src/main/java/dev/zenith/pearlplus/PearlPlusConfig.java
@@ -0,0 +1,10 @@
+package dev.zenith.pearlplus;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+public class PearlPlusConfig {
+    public boolean enabled = true;
+    public Map<String, List<String>> allowed = new LinkedHashMap<>();
+}
