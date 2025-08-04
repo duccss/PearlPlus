f10750dbaa9965854bee6f8b2cf855ba6b8513f8
duccss <123850042+duccss@users.noreply.github.com>
2025-08-04 10:05:31 +0000

    Update PearlPlusConfig.java
    uuid

--- src/main/java/dev/zenith/pearlplus/PearlPlusConfig.java
+++ src/main/java/dev/zenith/pearlplus/PearlPlusConfig.java
@@ -3,8 +3,9 @@ package dev.zenith.pearlplus;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 public class PearlPlusConfig {
     public boolean enabled = true;
-    public Map<String, List<String>> allowed = new LinkedHashMap<>();
+    public Map<UUID, List<String>> allowed = new LinkedHashMap<>();
 }
